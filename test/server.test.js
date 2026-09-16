7cdee4d8727316f6b821d8c6287132e683a96ffb
nbaldwin098 <kiddnixk@gmail.com>
2026-09-16 07:20:12 +0100

    Test: Trust footer link removed from labs homepage

--- test/server.test.js
+++ test/server.test.js
@@ -268,7 +268,7 @@ describe('host-based pages', () => {
     assert.match(res.body, /href="\/console"/);
     assert.match(res.body, /company-line/);
     assert.match(res.body, /og:image" content="https:\/\/aorilalabs\.com\/logo-aorila\.png"/);
-    assert.match(res.body, />Trust<\/a>/);
+    assert.doesNotMatch(res.body, />Trust<\/a>/);
     assert.match(res.body, /href="\/tp"/);
     assert.match(res.body, /href="https:\/\/dashboard\.aorilalabs\.com\/#support"/);
     assert.doesNotMatch(res.body, /href="\/privacy"/);
